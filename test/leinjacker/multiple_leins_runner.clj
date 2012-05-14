89e9539246dc37b15df367d97160b04bb1816fdb
Tobias Crawley <tcrawley@gmail.com>
2012-05-14 15:32:26 -0400

    Fix runner to properly install and provide leinjacker.
    It currently installs leinjacker and runs 'lein deps' for lein1, both
    of which are a bit of a hack, but I can't get leinjacker and its
    dependencies on the sub-project classpath via other means.

--- test/leinjacker/multiple_leins_runner.clj
+++ test/leinjacker/multiple_leins_runner.clj
@@ -3,7 +3,9 @@
   {:author "Toby Crawley"}
   (:use midje.sweet)
   (:require [clojure.java.io    :as io]
-            [clojure.java.shell :as shell]))
+            [clojure.java.shell :as shell]
+            [leinjacker.utils   :as utils]
+            [leiningen.install  :as install]))
 
 (defn- memoize-to-file
   "Memoizes the result if not-found-fn under key in filename."
@@ -36,7 +38,7 @@
   "Runs the midje command for the given generation of lein in test-project/"
   [generation]
   (let [cmd (find-lein-cmd generation)]
-    (println "==> Running" (str \' cmd " midje'") " in test-project/")
+    (println "==> Running" (str \' cmd " midje'") "in test-project/")
     (let [result (shell/sh cmd "midje"
                            :dir "test-project"
                            :env (assoc (into {} (System/getenv))
@@ -46,7 +48,12 @@
       (println "==> Done\n")
       (:exit result))))
 
+;; install leinjacker for the test project to use
+(install/install (utils/read-lein-project))
+
 (fact "run lein1 tests."
+  ;; lein1 needs deps resolved to see the installed leinjacker
+  (shell/sh (find-lein-cmd "1") "deps" :dir "test-project")
   (run-lein-on-sub-project "1") => 0)
 
 (fact "run lein2 tests."
