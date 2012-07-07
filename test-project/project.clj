7cbb93ade431db8edb0aa004b798caa2b13968de
Tobias Crawley <tcrawley@gmail.com>
2012-07-07 00:54:13 -0400

    Include lein-midje as a dependency for test-project.

--- test-project/project.clj
+++ test-project/project.clj
@@ -4,10 +4,12 @@
   :eval-in-leiningen true
   
   ;; lein 1
-  :dev-dependencies [[midje "1.3.1"]
+  :dev-dependencies [[lein-midje "1.0.9"]
+                     [midje "1.3.1"]
                      [leinjacker ~(nth (read-string (slurp "../project.clj")) 2)]]
   ;; lein 2
-  :profiles {:dev {:dependencies [[midje "1.3.1"]
+  :profiles {:dev {:dependencies [[lein-midje "2.0.0-SNAPSHOT"]
+                                  [midje "1.3.1"]
                                   [leinjacker ~(nth (read-string (slurp "../project.clj")) 2)]]}
              :foo {:some-key :some-value}})
 
