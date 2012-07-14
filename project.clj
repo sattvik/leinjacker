6d7fe66c99e9c2147e1fd3869930340a42249baa
Daniel Solano Gómez <daniel@solanogomez.org>
2012-07-14 12:59:35 -0500

    0.2.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject leinjacker "0.2.0-SNAPSHOT"
+(defproject leinjacker "0.2.0"
   :description "A library for Leiningen plug-in authors."
   :url "https://github.com/sattvik/leinjacker"
   :license {:name "Eclipse Public License"
