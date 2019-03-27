035d19dd9e9fa6e61c576a2cff8708952170951d
Toby Crawley <toby@tcrawley.org>
2019-03-27 06:21:15 -0400

    Version 0.4.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject leinjacker "0.4.3-SNAPSHOT"
+(defproject leinjacker "0.4.3"
   :description "A library for Leiningen plug-in authors."
   :url "https://github.com/sattvik/leinjacker"
   :license {:name "Eclipse Public License"
